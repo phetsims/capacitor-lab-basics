b15e625ae9ca1ab84c2b41cba4a0af4d4a852d43
Jesse <jesse.greenberg@colorado.edu>
2015-06-24 12:30:41 -0600

    Initial LightBulbModel

---
+++ js/common/model/LightBulbModel.js
@@ -0,0 +1,45 @@
+// Copyright 2002-2015, University of Colorado Boulder
+
+/**
+ * Model of a lightBulb, used in the Capacitor Lab: Basics sim.
+ *
+ * @author Jesse Greenberg
+ */
+
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var inherit = require( 'PHET_CORE/inherit' );
+  var PropertySet = require( 'AXON/PropertySet' );
+  var Vector3 = require( 'DOT/Vector3' );
+  var CLConstants = require( 'CAPACITOR_LAB_BASICS/common/CLConstants' );
+
+  /**
+   * Constructor for the Capacitor.
+   * @param {Vector3} location
+   * @constructor
+   */
+  function LightBulbModel( location ) {
+
+    // immutable variables.
+    this.location = location;
+
+    PropertySet.call( this, {
+      /* Populate with required properties */
+    } );
+  }
+  return inherit( PropertySet, LightBulbModel, {
+
+    /**
+     *
+     */
+    getTopConnectionPoint: function() {
+      return new Vector3( this.location.x, this.location.y - CLConstants.PLATE_SEPARATION_RANGE.max / 2, this.location.z );
+    },
+
+    getBottomConnectionPoint: function() {
+      return new Vector3( this.location.x, this.location.y + CLConstants.PLATE_SEPARATION_RANGE.max / 2, this.location.z );
+    }
+  } );
+} );
