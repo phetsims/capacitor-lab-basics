5d71c450bed0dc6ec3ce6c82cae7807e3c225711
Sam Reid <reids@colorado.edu>
2015-08-07 20:53:43 -0600

    Fixed require var names, see https://github.com/phetsims/chipper/issues/195

--- js/light-bulb/CapacitorLabBasicsLightBulbScreen.js
+++ js/light-bulb/CapacitorLabBasicsLightBulbScreen.js
@@ -9,7 +9,7 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var CapacitorLabBasicLightBulbsModel = require( 'CAPACITOR_LAB_BASICS/light-bulb/model/CapacitorLabBasicsLightBulbModel' );
+  var CapacitorLabBasicsLightBulbModel = require( 'CAPACITOR_LAB_BASICS/light-bulb/model/CapacitorLabBasicsLightBulbModel' );
   var CapacitorLabBasicsLightBulbScreenView = require( 'CAPACITOR_LAB_BASICS/light-bulb/view/CapacitorLabBasicsLightBulbScreenView' );
   var CLModelViewTransform3D = require( 'CAPACITOR_LAB_BASICS/common/model/CLModelViewTransform3D' );
   var inherit = require( 'PHET_CORE/inherit' );
@@ -28,7 +28,7 @@ define( function( require ) {
     var icon = new Rectangle( 0, 0, 548, 373, { fill: 'blue' } );
 
     Screen.call( this, capacitorLabBasicsLightBulbTitle, icon,
-      function() { return new CapacitorLabBasicLightBulbsModel( new CLModelViewTransform3D() ); },
+      function() { return new CapacitorLabBasicsLightBulbModel( new CLModelViewTransform3D() ); },
       function( model ) { return new CapacitorLabBasicsLightBulbScreenView( model ); },
       { backgroundColor: 'rgb( 151, 208, 255 )' }
     );
