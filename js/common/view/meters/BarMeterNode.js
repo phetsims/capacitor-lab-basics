5f067e3a0a1857c2df8eea02500aff4103dd1790
Jesse <jesse.greenberg@colorado.edu>
2015-10-09 09:24:30 -0600

    Reduce size of the bar meters so that they do not reach panel bounds

--- js/common/view/meters/BarMeterNode.js
+++ js/common/view/meters/BarMeterNode.js
@@ -27,7 +27,7 @@ define( function( require ) {
 
   // constants
   var BASE_LINE_LENGTH = 25;
-  var BAR_SIZE = new Dimension2( 275, 18 );
+  var BAR_SIZE = new Dimension2( 245, 18 );
   var BASE_LINE_OFFSET = ( BASE_LINE_LENGTH - BAR_SIZE.height ) / 2;
   var BAR_STROKE_COLOR = 'black';
   var BAR_LINE_WIDTH = 1;
