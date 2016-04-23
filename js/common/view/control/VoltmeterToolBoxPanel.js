47d102bc26faed819cb3800ae184210491006e90
Jesse Greenberg <jesse.greenberg@colorado.edu>
2016-04-23 09:08:04 -0600

    Voltmeter icon uses startDrag of Voltmeter body drag hanlder, see #62

--- js/common/view/control/VoltmeterToolBoxPanel.js
+++ js/common/view/control/VoltmeterToolBoxPanel.js
@@ -14,8 +14,6 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var VoltmeterNode = require( 'CAPACITOR_LAB_BASICS/common/view/meters/VoltmeterNode' );
   var Panel = require( 'SUN/Panel' );
-  var SimpleDragHandler = require( 'SCENERY/input/SimpleDragHandler' );
-  var ScreenView = require( 'JOIST/ScreenView' );
   var Vector2 = require( 'DOT/Vector2' );
   var CLBConstants = require( 'CAPACITOR_LAB_BASICS/common/CLBConstants' );
   var capacitorLabBasics = require( 'CAPACITOR_LAB_BASICS/capacitorLabBasics' );
@@ -37,54 +35,24 @@ define( function( require ) {
     // create the icon for the toolbox.
     var voltmeterIconNode = VoltmeterNode.VoltmeterIconNode();
 
-    var parentScreenView = null; // needed for coordinate transforms
-    var toolBoxDragHandler = new SimpleDragHandler( {
 
-      start: function( event ) {
-
-        // pull the voltmeter out of the toolbox
-        inUserControlProperty.set( true );
+    voltmeterIconNode.addInputListener( {
+      down: function( event ) {
         voltmeterVisibleProperty.set( true );
 
-        if ( !parentScreenView ) {
-          // find the root parent screenView
-          var testNode = thisToolBoxPanel;
-          while ( testNode !== null ) {
-            if ( testNode instanceof ScreenView ) {
-              parentScreenView = testNode;
-              break;
-            }
-            testNode = testNode.parents[ 0 ]; // Move up the scene graph by one level
-          }
-          assert && assert( parentScreenView, 'unable to find parent screen view' );
-        }
-
         // initial position of the pointer in the screenView coordinates
-        var initialPosition = parentScreenView.globalToLocalPoint( event.pointer.point );
+        var initialPosition = thisToolBoxPanel.globalToParentPoint( event.pointer.point );
 
-        // make sure that the center of the voltmeter body is offset for the
+        // make sure that the center of the voltmeter body is offset by the body dimensions
         var offsetPosition = new Vector2( -voltmeterNode.bodyNode.width / 2, -voltmeterNode.bodyNode.height / 2 );
 
-        // position of the  electricPotential sensor in ScreenView coordinates
         var voltmeterBodyPosition = initialPosition.plus( offsetPosition );
         voltmeterNode.bodyNode.bodyLocationProperty.set( modelViewTransform.viewToModelPosition( voltmeterBodyPosition ) );
-      },
 
-      end: function( event ) {
-        // if the bounds of the voltmeter body intersects the tool box
-        if ( thisToolBoxPanel.bounds.intersectsBounds( voltmeterNode.bodyNode.bounds.eroded( 40 ) ) ) {
-          voltmeterVisibleProperty.set( false );
-        }
-      },
-
-      translate: function( translationParams ) {
-        // TODO: restrict dragging to the screenView bounds
-        var unconstrainedLocation = voltmeterNode.bodyNode.bodyLocationProperty.value.plus( modelViewTransform.viewToModelDelta( translationParams.delta ) );
-        voltmeterNode.bodyNode.bodyLocationProperty.set( unconstrainedLocation );
+        // start drag from the body node's movable drag handler
+        voltmeterNode.bodyNode.movableDragHandler.startDrag( event );
       }
-
     } );
-    voltmeterIconNode.addInputListener( toolBoxDragHandler );
 
     // wrap all off this content inside of a node that will hold the input element and its descriptions
     Node.call( this );
