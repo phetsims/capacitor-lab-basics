phetsims/capacitor-lab-basics

commit 59b9301a06ece90dc728c961236061b2a967bcfa

Instrument for phet-io; heavy refactor of CircuitSwitch and SwitchedCapacitor (#50)

js/common/model/circuit/ParallelCircuit.js
@@ -274,26 +274,20 @@ define( function( require ) {
     var x = config.batteryLocation.x + config.capacitorXSpacing;
     var y = config.batteryLocation.y + config.capacitorYSpacing;
     var z = config.batteryLocation.z;
+    var location = new Vector3( x, y, z );
 
     var circuitComponents = [];
 
-    // create the capacitors
-    var location;
-    for ( var i = 0; i < config.numberOfCapacitors; i++ ) {
-      location = new Vector3( x, y, z );
-      var capacitor = new SwitchedCapacitor( location, config, config.circuitConnections, circuitConnectionProperty,
-        tandem, {
-          plateWidth: config.plateWidth,
-          plateSeparation: config.plateSeparation,
-          dielectricMaterial: config.dielectricMaterial,
-          dielectricOffset: config.dielectricOffset
-        } );
+    assert && assert( config.numberOfCapacitors === 1,
+      'Capacitor Lab: Basics is designed to use exactly one capacitor' );
 
-      circuitComponents.push( capacitor );
-      x += config.lightBulbXSpacing;
-    }
+    var capacitor = new SwitchedCapacitor( config, circuitConnectionProperty,
+      tandem ? tandem.createTandem( 'switchedCapacitor' ) : null );
+
+    circuitComponents.push( capacitor );
 
     // create the light bulbs
+    var i;
     for ( i = 0; i < config.numberOfLightBulbs; i++ ) {
       location = new Vector3( x, y, z );
       var lightBulb = new LightBulb( location, config.lightBulbResistance );

js/common/model/Capacitor.js
@@ -36,6 +36,7 @@ define( function( require ) {
    * @param {Vector3} location
    * @param {CLBModelViewTransform3D} modelViewTransform
    * @param {Tandem} tandem
+   * @param {Object} [options]
    * @constructor
    */
   function Capacitor( location, modelViewTransform, tandem, options ) {

js/common/model/CircuitSwitch.js
@@ -1,4 +1,4 @@
-// Copyright 2015, University of Colorado Boulder
+// Copyright 2016, University of Colorado Boulder
 
 /**
  * Circuit switch.  The circuit switch has a start point and an end point.  The start point acts as a hinge, and
@@ -19,9 +19,6 @@ define( function( require ) {
   var PropertySet = require( 'AXON/PropertySet' );
   var capacitorLabBasics = require( 'CAPACITOR_LAB_BASICS/capacitorLabBasics' );
 
-  // constants
-  var SWITCH_ANGLE = Math.PI / 4; // angle from the vertical of each connection point
-
   /**
    * Constructor for a CircuitSwitch.
    *
@@ -31,7 +28,8 @@ define( function( require ) {
    * @param {Property} circuitConnectionProperty,
    * @param {string} connectionLocation
    */
-  function CircuitSwitch( hingePoint, connections, modelViewTransform, circuitConnectionProperty, connectionLocation ) {
+  function CircuitSwitch( hingePoint, connections, modelViewTransform,
+    circuitConnectionProperty, connectionLocation, tandem ) {
 
     // @private
     this.initialAngle = 0; // with respect to the vertical ( open switch )
@@ -47,6 +45,10 @@ define( function( require ) {
 
     PropertySet.call( this, {
       angle: this.initialAngle
+    }, {
+      tandemSet: {
+        angle: tandem ? tandem.createTandem( 'angleProperty' ) : null
+      }
     } );
 
     // add the switch wire that spans two connection points. Default connection is to the battery.
@@ -63,7 +65,7 @@ define( function( require ) {
 
   capacitorLabBasics.register( 'CircuitSwitch', CircuitSwitch );
 
-  inherit( PropertySet, CircuitSwitch, {
+  return inherit( PropertySet, CircuitSwitch, {
 
     /**
      * Get the desired connection from the connection type.
@@ -94,17 +96,6 @@ define( function( require ) {
 
       var connection = this.getConnection( connectionType );
       return connection.location.toVector2();
-
-      // var returnConnectionPoint;
-      // this.connections.forEach( function( connection ) {
-      //   if ( connection.connectionType === connectionType ) {
-      //     returnConnectionPoint = connection.location;
-      //   }
-      // } );
-      // if ( returnConnectionPoint === 'undefined' ) {
-      //   console.error( 'Requested connection type that does not exist for this circuit.' );
-      // }
-      // return returnConnectionPoint.toVector2();
     },
 
     /**
@@ -153,135 +144,6 @@ define( function( require ) {
     getLeftLimitAngle: function() {
       return this.getConnectionPoint( CircuitConnectionEnum.BATTERY_CONNECTED ).minus( this.hingePoint.toVector2() ).angle();
     }
-  }, {
-
-    // Create specific types for the circuit switches
-    /**
-     * Create a circuit switch for the top of a capacitor.
-     *
-     * @param {Vector3} hingePoint
-     * @param {CLBModelViewTransform3D} modelViewTransform
-     * @param {Array<string>} possibleConnections - possible connection types from CircuitConnectionEnum entries
-     * @param {Property.<string>} circuitConnectionProperty
-     */
-    CircuitTopSwitch: function( hingePoint, modelViewTransform, possibleConnections, circuitConnectionProperty ) {
-      return new CircuitTopSwitch( hingePoint, modelViewTransform, possibleConnections, circuitConnectionProperty );
-    },
-
-    /**
-     * Create a circuit switch for the bottom of a capacitor.
-     *
-     * @param {Vector3} hingePoint
-     * @param {CLBModelViewTransform3D} modelViewTransform
-     * @param {Array<string>} possibleConnections possible connection types from CircuitConnectionEnum entries
-     * @param {Property.<string>} circuitConnectionProperty
-     */
-    CircuitBottomSwitch: function( hingePoint, modelViewTransform, possibleConnections, circuitConnectionProperty ) {
-      return new CircuitBottomSwitch( hingePoint, modelViewTransform, possibleConnections, circuitConnectionProperty );
-    }
   } );
-
-
-  /**
-   * Constructor for a switch connected to the top of a capacitor.
-   *
-   * @param {Vector3} hingePoint
-   * @param {CLModelViewTransform3} modelViewTransform
-   * @param {Property.<string>} circuitConnectionProperty
-   * @constructor
-   */
-  function CircuitTopSwitch( hingePoint, modelViewTransform, circuitSwitchConnections, circuitConnectionProperty ) {
-
-    // calculate the locations of the connection points for this circuit
-    var topPoint = hingePoint.toVector2().minusXY( 0, CLBConstants.SWITCH_WIRE_LENGTH );
-    var leftPoint = hingePoint.toVector2().minusXY(
-      CLBConstants.SWITCH_WIRE_LENGTH * Math.sin( SWITCH_ANGLE ),
-      CLBConstants.SWITCH_WIRE_LENGTH * Math.cos( SWITCH_ANGLE )
-    );
-    var rightPoint = hingePoint.toVector2().minusXY( -CLBConstants.SWITCH_WIRE_LENGTH * Math.sin( SWITCH_ANGLE ),
-      CLBConstants.SWITCH_WIRE_LENGTH * Math.cos( SWITCH_ANGLE )
-    );
-
-    // collect location and connection type for each of the possible circuit switch connections
-    var connections = [];
-    circuitSwitchConnections.forEach( function( circuitSwitchConnection ) {
-      if ( circuitSwitchConnection === CircuitConnectionEnum.OPEN_CIRCUIT ) {
-        connections.push( {
-          location: topPoint.toVector3(),
-          connectionType: circuitSwitchConnection
-        } );
-      } else if ( circuitSwitchConnection === CircuitConnectionEnum.BATTERY_CONNECTED ) {
-        connections.push( {
-          location: leftPoint.toVector3(),
-          connectionType: circuitSwitchConnection
-        } );
-      } else if ( circuitSwitchConnection === CircuitConnectionEnum.LIGHT_BULB_CONNECTED ) {
-        connections.push( {
-          location: rightPoint.toVector3(),
-          connectionType: circuitSwitchConnection
-        } );
-      } else {
-        assert && assert( 'attempting to create switch conection which is not supported' );
-      }
-    } );
-
-    CircuitSwitch.call( this, hingePoint, connections, modelViewTransform, circuitConnectionProperty, CLBConstants.WIRE_CONNECTIONS.CIRCUIT_SWITCH_TOP );
-  }
-
-  capacitorLabBasics.register( 'CircuitTopSwitch', CircuitTopSwitch );
-
-  inherit( CircuitSwitch, CircuitTopSwitch );
-
-  /**
-   * Constructor for a switch connected to the bottom of a capacitor.
-   *
-   * @param {Vector3} hingePoint
-   * @param {CLModelViewTransform3} modelViewTransform
-   * @param {Property.<string>} circuitConnectionProperty
-   * @constructor
-   */
-  function CircuitBottomSwitch( hingePoint, modelViewTransform, circuitSwitchConnections, circuitConnectionProperty ) {
-
-    // determine the locations of each connection point
-    var topPoint = hingePoint.toVector2().plusXY( 0, CLBConstants.SWITCH_WIRE_LENGTH );
-    var rightPoint = hingePoint.toVector2().plusXY(
-      CLBConstants.SWITCH_WIRE_LENGTH * Math.sin( SWITCH_ANGLE ),
-      CLBConstants.SWITCH_WIRE_LENGTH * Math.cos( SWITCH_ANGLE )
-    );
-    var leftPoint = hingePoint.toVector2().plusXY( -CLBConstants.SWITCH_WIRE_LENGTH * Math.sin( SWITCH_ANGLE ),
-      CLBConstants.SWITCH_WIRE_LENGTH * Math.cos( SWITCH_ANGLE )
-    );
-
-    // collect location and connection type for each of the possible circuit switch connections
-    var connections = [];
-    circuitSwitchConnections.forEach( function( circuitSwitchConnection ) {
-      if ( circuitSwitchConnection === CircuitConnectionEnum.OPEN_CIRCUIT ) {
-        connections.push( {
-          location: topPoint.toVector3(),
-          connectionType: circuitSwitchConnection
-        } );
-      } else if ( circuitSwitchConnection === CircuitConnectionEnum.BATTERY_CONNECTED ) {
-        connections.push( {
-          location: leftPoint.toVector3(),
-          connectionType: circuitSwitchConnection
-        } );
-      } else if ( circuitSwitchConnection === CircuitConnectionEnum.LIGHT_BULB_CONNECTED ) {
-        connections.push( {
-          location: rightPoint.toVector3(),
-          connectionType: circuitSwitchConnection
-        } );
-      } else {
-        assert && assert( 'attempting to create switch conection which is not supported' );
-      }
-    } );
-
-    CircuitSwitch.call( this, hingePoint, connections, modelViewTransform, circuitConnectionProperty, CLBConstants.WIRE_CONNECTIONS.CIRCUIT_SWITCH_BOTTOM );
-  }
-
-  capacitorLabBasics.register( 'CircuitBottomSwitch', CircuitBottomSwitch );
-
-  inherit( CircuitSwitch, CircuitBottomSwitch );
-
-  return CircuitSwitch;
 } );
 

js/common/model/SwitchedCapacitor.js
@@ -9,36 +9,37 @@ define( function( require ) {
   // modules
   var inherit = require( 'PHET_CORE/inherit' );
   var capacitorLabBasics = require( 'CAPACITOR_LAB_BASICS/capacitorLabBasics' );
-  var DielectricMaterial = require( 'CAPACITOR_LAB_BASICS/common/model/DielectricMaterial' );
   var CLBConstants = require( 'CAPACITOR_LAB_BASICS/common/CLBConstants' );
   var Capacitor = require( 'CAPACITOR_LAB_BASICS/common/model/Capacitor' );
   var Vector3 = require( 'DOT/Vector3' );
   var CircuitSwitch = require( 'CAPACITOR_LAB_BASICS/common/model/CircuitSwitch' );
+  var CircuitConnectionEnum = require( 'CAPACITOR_LAB_BASICS/common/model/CircuitConnectionEnum' );
+
+  // constants
+  var SWITCH_ANGLE = Math.PI / 4; // angle from the vertical of each connection point
 
   /**
-   * Constructor.
-   *
-   * @param {Vector3} location
-   * @param {number} plateWidth
-   * @param {number} plateSeparation
-   * @param {DielectricMaterial} dielectricMaterial
-   * @param {number} dielectricOffset
-   * @param {CLModelViewTransform3D} modelViewTransform
+   * @param {CircuitConfig} config
+   * @param {Property.<string>} circuitConnectionProperty
    * @param {Tandem} tandem
-   * @param {Object} options
    */
-  function SwitchedCapacitor( location, config, switchConnections, circuitConnectionProperty, tandem, options ) {
+  function SwitchedCapacitor( config, circuitConnectionProperty, tandem ) {
 
-    // options that populate the capacitor with various geometric and dielectric properties
-    options = _.extend( {
-      plateWidth: CLBConstants.PLATE_WIDTH_RANGE.min,
-      plateSeparation: CLBConstants.PLATE_SEPARATION_RANGE.max,
-      dielectricMaterial: DielectricMaterial.AIR,
-      dielectricOffset: CLBConstants.PLATE_WIDTH_RANGE.max + 1
-    }, options );
+    var location = new Vector3(
+      config.batteryLocation.x + config.capacitorXSpacing,
+      config.batteryLocation.y + config.capacitorYSpacing,
+      config.batteryLocation.z );
 
-    // create the capacitor
+    // TODO: Remove options from Capacitor constructor and just use config
+    var options = {
+      plateWidth: config.plateWidth,
+      plateSeparation: config.plateSeparation,
+      dielectricMaterial: config.dielectricMaterial,
+      dielectricOffset: config.dielectricOffset
+    };
     Capacitor.call( this, location, config.modelViewTransform, tandem, options );
+
+    this.config = config;
 
     // create the circuit switches that connect the capacitor to the circuit
     var x = config.batteryLocation.x + config.capacitorXSpacing;
@@ -49,12 +50,91 @@ define( function( require ) {
     var topStartPoint = new Vector3( x, topY, z );
     var bottomStartPoint = new Vector3( x, bottomY, z );
 
-    this.topCircuitSwitch = CircuitSwitch.CircuitTopSwitch( topStartPoint, config.modelViewTransform, switchConnections, circuitConnectionProperty );
-    this.bottomCircuitSwitch = CircuitSwitch.CircuitBottomSwitch( bottomStartPoint, config.modelViewTransform, switchConnections, circuitConnectionProperty );
+    var topConnections = this.getTopConnections( topStartPoint );
+    var bottomConnections = this.getBottomConnections( bottomStartPoint );
 
+    this.topCircuitSwitch = new CircuitSwitch( topStartPoint, topConnections, config.modelViewTransform,
+      circuitConnectionProperty, CLBConstants.WIRE_CONNECTIONS.CIRCUIT_SWITCH_TOP,
+      tandem ? tandem.createTandem( 'topCircuitSwitch' ) : null );
+    this.bottomCircuitSwitch = new CircuitSwitch( bottomStartPoint, bottomConnections, config.modelViewTransform,
+      circuitConnectionProperty, CLBConstants.WIRE_CONNECTIONS.CIRCUIT_SWITCH_BOTTOM,
+      tandem ? tandem.createTandem( 'bottomCircuitSwitch' ) : null );
   }
 
   capacitorLabBasics.register( 'SwitchedCapacitor', SwitchedCapacitor );
 
-  return inherit( Capacitor, SwitchedCapacitor, {} );
+  return inherit( Capacitor, SwitchedCapacitor, {
+
+    // calculate the locations of the connection points for this circuit
+    getTopConnections: function( hingePoint ) {
+      var topPoint = hingePoint.toVector2().minusXY( 0, CLBConstants.SWITCH_WIRE_LENGTH );
+      var leftPoint = hingePoint.toVector2().minusXY(
+        CLBConstants.SWITCH_WIRE_LENGTH * Math.sin( SWITCH_ANGLE ),
+        CLBConstants.SWITCH_WIRE_LENGTH * Math.cos( SWITCH_ANGLE )
+      );
+      var rightPoint = hingePoint.toVector2().minusXY( -CLBConstants.SWITCH_WIRE_LENGTH * Math.sin( SWITCH_ANGLE ),
+        CLBConstants.SWITCH_WIRE_LENGTH * Math.cos( SWITCH_ANGLE )
+      );
+
+      var connections = this.getConnectionLocationsAndTypes( this.config.circuitConnections,
+        topPoint, leftPoint, rightPoint );
+      return connections;
+    },
+
+    // determine the locations of each connection point
+    getBottomConnections: function( hingePoint ) {
+      var topPoint = hingePoint.toVector2().plusXY( 0, CLBConstants.SWITCH_WIRE_LENGTH );
+      var rightPoint = hingePoint.toVector2().plusXY(
+        CLBConstants.SWITCH_WIRE_LENGTH * Math.sin( SWITCH_ANGLE ),
+        CLBConstants.SWITCH_WIRE_LENGTH * Math.cos( SWITCH_ANGLE )
+      );
+      var leftPoint = hingePoint.toVector2().plusXY( -CLBConstants.SWITCH_WIRE_LENGTH * Math.sin( SWITCH_ANGLE ),
+        CLBConstants.SWITCH_WIRE_LENGTH * Math.cos( SWITCH_ANGLE )
+      );
+
+      var connections = this.getConnectionLocationsAndTypes( this.config.circuitConnections,
+        topPoint, leftPoint, rightPoint );
+      return connections;
+    },
+
+    /**
+     * Return array of objects containing location (Vector3) and type (CircuitConnectionEnum value)
+     * for each connection in circuitSwitchConnections.
+     *
+     * @param {Array<Object>} connections - array of { location: Vector3, connectionType: string }
+     * @param  {Vector2} topPoint
+     * @param  {Vector2} leftPoint
+     * @param  {Vector2} rightPoint
+     *
+     * @return {Array<Object>} - array of { location: Vector3, connectionType: string }
+     */
+    getConnectionLocationsAndTypes: function( circuitSwitchConnections, topPoint, leftPoint, rightPoint ) {
+
+      // collect location and connection type for each of the possible circuit switch connections
+      var connections = [];
+      circuitSwitchConnections.forEach( function( circuitSwitchConnection ) {
+        if ( circuitSwitchConnection === CircuitConnectionEnum.OPEN_CIRCUIT ) {
+          connections.push( {
+            location: topPoint.toVector3(),
+            connectionType: circuitSwitchConnection
+          } );
+        } else if ( circuitSwitchConnection === CircuitConnectionEnum.BATTERY_CONNECTED ) {
+          connections.push( {
+            location: leftPoint.toVector3(),
+            connectionType: circuitSwitchConnection
+          } );
+        } else if ( circuitSwitchConnection === CircuitConnectionEnum.LIGHT_BULB_CONNECTED ) {
+          connections.push( {
+            location: rightPoint.toVector3(),
+            connectionType: circuitSwitchConnection
+          } );
+        } else {
+          assert && assert( 'attempting to create switch conection which is not supported' );
+        }
+      } );
+      return connections;
+    }
+
+  } );
 } );
+

commit 147310a1b08c8e2b1720d28fa0d3bd0b3d9d8e51

Fix layout bug introduced in 59b9301a06ece90dc728c961236061b2a967bcfa

js/common/model/circuit/ParallelCircuit.js
@@ -288,6 +288,7 @@ define( function( require ) {
 
     // create the light bulbs
     var i;
+    x += config.capacitorXSpacing;
     for ( i = 0; i < config.numberOfLightBulbs; i++ ) {
       location = new Vector3( x, y, z );
       var lightBulb = new LightBulb( location, config.lightBulbResistance );