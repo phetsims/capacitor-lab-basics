3400108656c68c39cabb74577bc45df6d5c5a9e5
Andrew Adare <andrewadare@gmail.com>
2016-12-02 14:11:29 -0700

    Remove non-vacuum dielectric variables from CLBConstants https://github.com/phetsims/capacitor-lab-basics/issues/178

--- js/common/CLBConstants.js
+++ js/common/CLBConstants.js
@@ -15,9 +15,6 @@ define( function( require ) {
   var PhetColorScheme = require( 'SCENERY_PHET/PhetColorScheme' );
   var capacitorLabBasics = require( 'CAPACITOR_LAB_BASICS/capacitorLabBasics' );
 
-  // constants
-  var PLATE_WIDTH_RANGE = new RangeWithValue( 0.01, 0.02, 0.01 ); // meters
-
   var CLBConstants = {
 
     //----------------------------------------------------------------------------
@@ -59,35 +56,8 @@ define( function( require ) {
     SWITCH_WIRE_LENGTH: 0.0064, // in meters
     SWITCH_Y_SPACING: 0.0025, // spacing between circuit components and the switch
 
-    // dielectric
-    DIELECTRIC_CONSTANT_RANGE: new RangeWithValue( 1, 5, 5 ), // dimensionless
-    DIELECTRIC_OFFSET_RANGE: new RangeWithValue( 0, PLATE_WIDTH_RANGE.max, PLATE_WIDTH_RANGE.defaultValue ), // meters
-
     // dielectric constants (dimensionless)
-    //REVIEW: Vacuum should be kept, rest are not needed (see https://github.com/phetsims/capacitor-lab-basics/issues/178)
     EPSILON_VACUUM: 1,
-    EPSILON_GLASS: 4.7,
-    EPSILON_PAPER: 3.5,
-    EPSILON_TEFLON: 2.1,
-
-    /*
-     * The dielectric constant of air is actually 1.0005896. The model for this sim specified that
-     * the circuit was in air.  But we discovered late in the development of this sim that we should
-     * have modeled the circuit in a vacuum, because we want the E-Field component due to the
-     * environment to be zero.  With air, we have a small Dielectric vector of up to 4 V/m shown
-     * on the E-Field Detector when the Plate Charge control is set to its maximum.
-     *
-     * Rather than change "air" to "vacuum" in numerous places throughout the code and design doc,
-     * it was suggested that we simply set the dielectric constant of air to 1.0.  I was hesitant to
-     * do this, since I think it's going to cause future problems.  But Kathy P. bet me a 6-pack of
-     * beer that it will never be a problem.  Any developer who needs to change this in the future
-     * is hereby bound by the Developer Code of Ethics to inform me, so that I can collect on
-     * this wager.
-     * REVIEW: why are we getting air capacitance AND dielectric capacitance?
-     * REVIEW: Since we're not handling dielectrics in this sim, it seems simpler to replace AIR epsilon with a vaccuum.
-     *         Andrew, should CM be contacted?
-     */
-    EPSILON_AIR: 1.0,
 
     // Wire
     WIRE_THICKNESS: 0.0005, // meters
@@ -144,10 +114,6 @@ define( function( require ) {
     STORED_ENERGY_COLOR: 'yellow',
     POSITIVE_CHARGE_COLOR: PhetColorScheme.RED_COLORBLIND,
     NEGATIVE_CHARGE_COLOR: 'blue',
-    //REVIEW: Pretty sure this custom color is never visible in the sim, recommend removal of color in general
-    CUSTOM_DIELECTRIC_COLOR: 'rgb( 255, 255, 125 )', // pale yellow
-    //REVIEW: Pretty sure this custom color is never visible in the sim, recommend removal of color in general
-    AIR_COLOR: 'rgba( 255, 0, 0, 1 )', // This should never be seen so pick something obviously wrong.
 
     // other common colors
     METER_PANEL_FILL: 'rgb( 255, 245, 237)',
