9ce92c32ac66cfe26320bf75e76b2234bf48c12f
Andrew Adare <andrewadare@gmail.com>
2016-12-09 12:34:53 -0700

    Review items for BarMeter - #117

--- js/common/model/meter/BarMeter.js
+++ js/common/model/meter/BarMeter.js
@@ -1,10 +1,11 @@
-// Copyright 2015, University of Colorado Boulder
+// Copyright 2016, University of Colorado Boulder
 
 /**
  * Base class for all bar meter model elements.
  *
  * @author Chris Malley (cmalley@pixelzoom.com)
  * @author Jesse Greenberg
+ * @author Andrew Adare
  */
 define( function( require ) {
   'use strict';
@@ -12,6 +13,7 @@ define( function( require ) {
   // modules
   var capacitorLabBasics = require( 'CAPACITOR_LAB_BASICS/capacitorLabBasics' );
   var inherit = require( 'PHET_CORE/inherit' );
+  var NumberProperty = require( 'AXON/NumberProperty' );
   var Property = require( 'AXON/Property' );
 
   // phet-io modules
@@ -28,11 +30,10 @@ define( function( require ) {
    */
   function BarMeter( circuit, visibleProperty, valueFunction, tandem ) {
 
-    //REVIEW: Use NumberProperty
     var meterValue = valueFunction( circuit );
     assert && assert( !_.isNaN( meterValue ), 'meterValue is ' + meterValue );
 
-    this.valueProperty = new Property( meterValue, {
+    this.valueProperty = new NumberProperty( meterValue, {
       tandem: tandem.createTandem( 'valueProperty' ),
       phetioValueType: TNumber()
     } );
@@ -44,10 +45,7 @@ define( function( require ) {
     this.circuit = circuit; // @private
     this.valueFunction = valueFunction; // @private
 
-    //REVIEW: Disposal not implemented.
-    //REVIEW: ParallelCircuit does not declare circuit.capacitor. What subtype dependencies does this actually have?
-    //        Update documentation to note the dependency.
-    // TODO implement disposal
+    // No need for disposal since the BarMeter is persistent
     Property.multilink( [
       circuit.capacitor.plateSizeProperty,
       circuit.capacitor.plateSeparationProperty,
@@ -60,12 +58,19 @@ define( function( require ) {
   capacitorLabBasics.register( 'BarMeter', BarMeter );
 
   return inherit( Object, BarMeter, {
-    //REVIEW: doc
+
+    /**
+     * Update meter reading
+     * @public
+     */
     updateValue: function() {
       this.valueProperty.set( this.valueFunction( this.circuit ) );
     },
 
-    //REVIEW: doc
+    /**
+     * Reset the BarMeter
+     * @public
+     */
     reset: function() {
       this.visibleProperty.reset();
     }
