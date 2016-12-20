phetsims/capacitor-lab-basics

commit 1261e84633e18da4cb37aaf6ae2a930d10a46b84

Plate intersection functions for Capacitor - #117

js/common/model/Capacitor.js
@@ -1,4 +1,4 @@
-// Copyright 2015, University of Colorado Boulder
+// Copyright 2016, University of Colorado Boulder
 
 /**
  * Model of a capacitor whose geometry can be directly manipulated by the user.
@@ -105,6 +105,9 @@ define( function( require ) {
     this.bottomCircuitSwitch = CircuitSwitch.BOTTOM( config, circuitConnectionProperty,
       tandem.createTandem( 'bottomCircuitSwitch' ) );
 
+    this.topPlateFaces = this.shapeCreator.createTopPlateShape();
+    this.bottomPlateFaces = this.shapeCreator.createBottomPlateShape();
+
     // link the top and bottom circuit switches together so that they rotate together
     // as the user drags
     var self = this;
@@ -193,20 +196,14 @@ define( function( require ) {
      * @returns {boolean}
      */
     intersectsTopPlate: function( shape ) {
-      var intersectsTopPlate = false;
-      //REVIEW: Why are we creating these shapes repeatedly? Presumably just create them on startup since they don't change.
-      //REVIEW: Use _.some()
-      this.shapeCreator.createTopPlateShape().forEach( function( topPlateShape ) {
 
-        // kite does not support finding the intersection between two shapes and checking for
-        // intersection of shape bounds is too inacurate in this case because of the 3d perspective.
-        // Determining if center of shape bounds is most accurate assuming the shape is small relative
-        // to the plate
-        if ( topPlateShape.containsPoint( shape.bounds.center ) ) {
-          intersectsTopPlate = true;
-        }
+      // kite does not support finding the intersection between two shapes and checking for
+      // intersection of shape bounds is too inacurate in this case because of the 3d perspective.
+      // Determining if center of shape bounds is most accurate assuming the shape is small relative
+      // to the plate
+      return _.some( this.topPlateFaces, function( plateShape ) {
+        plateShape.containsPoint( shape.bounds.center );
       } );
-      return intersectsTopPlate;
     },
 
     /**
@@ -218,15 +215,9 @@ define( function( require ) {
      * @returns {boolean}
      */
     intersectsBottomPlate: function( shape ) {
-      var intersectsBottomPlate = false;
-      //REVIEW: Why are we creating these shapes repeatedly? Presumably just create them on startup since they don't change.
-      //REVIEW: Use _.some()
-      this.shapeCreator.createBottomPlateShape().forEach( function( bottomPlateShape ) {
-        if ( bottomPlateShape.containsPoint( shape.bounds.center ) ) {
-          intersectsBottomPlate = true;
-        }
+      return _.some( this.bottomPlateFaces, function( plateShape ) {
+        plateShape.containsPoint( shape.bounds.center );
       } );
-      return intersectsBottomPlate;
     },
 
     /**

js/common/model/shapes/CapacitorShapeCreator.js
@@ -45,7 +45,7 @@ define( function( require ) {
      * Top plate, unoccluded.
      * @public
      *
-     * @returns {Shape[]} REVIEW: createBoxShapes() instead?
+     * @returns {createBoxShape()}
      */
     createTopPlateShape: function() {
       var x = this.capacitor.location.x;
@@ -60,7 +60,7 @@ define( function( require ) {
      * Bottom plate, unoccluded.
      * @public
      *
-     * @returns {Shape[]} REVIEW: createBoxShapes() instead?
+     * @returns {createBoxShape()}
      */
     createBottomPlateShape: function() {
       var x = this.capacitor.location.x;
@@ -83,7 +83,7 @@ define( function( require ) {
      * @param z
      * @param size
      *
-     * @returns {Shape[]} REVIEW: createBoxShapes() instead? REVIEW: createBoxShapes() instead?
+     * @returns {BoxShapeCreator.createBoxShape()}
      */
     createBoxShape: function( x, y, z, size ) {
       return this.boxShapeCreator.createBoxShape( x, y, z, size.width, size.height, size.depth );

commit 2be1a2b73725597caa167cfa810b6a5d133ca82d

Fix bug introduced in 1261e84 for #117. Plate shapes must be created dynamically. - see #185

js/common/model/Capacitor.js
@@ -142,11 +142,6 @@ define( function( require ) {
     this.bottomCircuitSwitch = CircuitSwitch.BOTTOM( config, circuitConnectionProperty,
       tandem.createTandem( 'bottomCircuitSwitch' ) );
 
-    // @private
-    this.plateFaces = {};
-    this.plateFaces[ CircuitLocation.CAPACITOR_TOP ] = this.shapeCreator.createTopPlateShape();
-    this.plateFaces[ CircuitLocation.CAPACITOR_BOTTOM ] = this.shapeCreator.createBottomPlateShape();
-
     // link the top and bottom circuit switches together so that they rotate together
     // as the user drags
     var self = this;
@@ -230,10 +225,21 @@ define( function( require ) {
      */
     contacts: function ( probe, location ) {
 
-      assert && assert( this.plateFaces.hasOwnProperty( location ), 'Invalid capacitor location: ' + location );
+      assert && assert( location === CircuitLocation.CAPACITOR_TOP || location === CircuitLocation.CAPACITOR_BOTTOM,
+        'Invalid capacitor location: ' + location );
 
-      return _.some( this.plateFaces[ location ], function( plateShape ) {
-        return plateShape.containsPoint( probe.bounds.center );
+      var faces = null;
+      if ( location === CircuitLocation.CAPACITOR_TOP ) {
+        faces = this.shapeCreator.createTopPlateShape();
+      }
+      else if ( location === CircuitLocation.CAPACITOR_BOTTOM ) {
+        faces = this.shapeCreator.createBottomPlateShape();
+      }
+
+      assert && assert( faces, 'Plate face shapes were not created for ' + location );
+
+      return _.some( faces, function( face ) {
+        return face.containsPoint( probe.bounds.center );
       } );
     },
 