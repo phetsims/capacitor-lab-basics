af3f174f8c1395e91a6e07f99693a54e9700c42a
phet-steele <steele.phet@gmail.com>
2017-11-02 11:59:57 -0600

    QA credits, #189

--- js/capacitor-lab-basics-main.js
+++ js/capacitor-lab-basics-main.js
@@ -29,7 +29,7 @@ define( function( require ) {
       leadDesign: 'Amy Rouinfar',
       softwareDevelopment: 'Andrew Adare, Jesse Greenberg, Chris Malley, Emily Randall, Jonathan Olson',
       team: 'Emily Moore, Ariel Paul, Kathy Perkins, Emily Randall',
-      qualityAssurance: 'Steele Dalton, Amanda Davis, Bryce Griebenow, Elise Morgan, Oliver Orejola, Ben Roberts, Bryan Yoelin',
+      qualityAssurance: 'Steele Dalton, Amanda Davis, Bryce Griebenow, Ethan Johnson, Elise Morgan, Oliver Orejola, Arnab Purkayastha, Ben Roberts, Clara Wilson, Bryan Yoelin',
     }
   };
 
