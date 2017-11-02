ecd1d88710a073c2c503540f46e86b46d95b45ff
Jonathan Olson <olsonsjc@gmail.com>
2017-11-02 13:37:42 -0600

    Adding connection point view for where the hit test should happen, see https://github.com/phetsims/capacitor-lab-basics/issues/197

--- js/common/view/DebugLayer.js
+++ js/common/view/DebugLayer.js
@@ -11,12 +11,14 @@ define( function( require ) {
   // modules
   var capacitorLabBasics = require( 'CAPACITOR_LAB_BASICS/capacitorLabBasics' );
   var CircuitLocation = require( 'CAPACITOR_LAB_BASICS/common/model/CircuitLocation' );
+  var CircuitState = require( 'CAPACITOR_LAB_BASICS/common/model/CircuitState' );
   var CLBConstants = require( 'CAPACITOR_LAB_BASICS/common/CLBConstants' );
   var CLBQueryParameters = require( 'CAPACITOR_LAB_BASICS/common/CLBQueryParameters' );
   var inherit = require( 'PHET_CORE/inherit' );
   var Node = require( 'SCENERY/nodes/Node' );
   var Path = require( 'SCENERY/nodes/Path' );
   var Property = require( 'AXON/Property' );
+  var Shape = require( 'KITE/Shape' );
 
   /**
    * @constructor
@@ -77,6 +79,40 @@ define( function( require ) {
           stroke: 'blue'
         } )
       ];
+    } );
+
+    var topSwitchContainer = new Node();
+    this.addChild( topSwitchContainer );
+    capacitor.topCircuitSwitch.circuitConnectionProperty.link( function( circuitConnection ) {
+      topSwitchContainer.removeAllChildren();
+
+      if ( circuitConnection !== CircuitState.SWITCH_IN_TRANSIT && circuitConnection !== CircuitState.OPEN_CIRCUIT ) {
+        var endPoint = capacitor.topCircuitSwitch.switchSegment.endPointProperty.value;
+        var hingePoint = capacitor.topCircuitSwitch.switchSegment.hingePoint;
+        var delta = endPoint.minus( hingePoint ).setMagnitude( CLBConstants.SWITCH_WIRE_LENGTH );
+
+        var point = model.modelViewTransform.modelToViewPosition( hingePoint.plus( delta ) );
+        topSwitchContainer.addChild( new Path( Shape.circle( point.x, point.y, CLBConstants.CONNECTION_POINT_RADIUS ), {
+          stroke: 'blue'
+        } ) );
+      }
+    } );
+
+    var bottomSwitchContainer = new Node();
+    this.addChild( bottomSwitchContainer );
+    capacitor.bottomCircuitSwitch.circuitConnectionProperty.link( function( circuitConnection ) {
+      bottomSwitchContainer.removeAllChildren();
+
+      if ( circuitConnection !== CircuitState.SWITCH_IN_TRANSIT && circuitConnection !== CircuitState.OPEN_CIRCUIT ) {
+        var endPoint = capacitor.bottomCircuitSwitch.switchSegment.endPointProperty.value;
+        var hingePoint = capacitor.bottomCircuitSwitch.switchSegment.hingePoint;
+        var delta = endPoint.minus( hingePoint ).setMagnitude( CLBConstants.SWITCH_WIRE_LENGTH );
+
+        var point = model.modelViewTransform.modelToViewPosition( hingePoint.plus( delta ) );
+        bottomSwitchContainer.addChild( new Path( Shape.circle( point.x, point.y, CLBConstants.CONNECTION_POINT_RADIUS ), {
+          stroke: 'blue'
+        } ) );
+      }
     } );
 
     var terminalContainer = new Node();
