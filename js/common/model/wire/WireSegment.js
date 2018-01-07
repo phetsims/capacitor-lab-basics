79fc03566e625d3f05886fedd6b9c1533bb0fb8e
samreid <reids@colorado.edu>
2018-01-07 08:19:42 -0700

    Updated copyright dates, see https://github.com/phetsims/chipper/issues/643

--- js/common/model/wire/WireSegment.js
+++ js/common/model/wire/WireSegment.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2017, University of Colorado Boulder
+// Copyright 2015-2018, University of Colorado Boulder
 
 /**
  * A straight segment of wire. One or more segments are joined to create a Wire.  Contains factory functions to
