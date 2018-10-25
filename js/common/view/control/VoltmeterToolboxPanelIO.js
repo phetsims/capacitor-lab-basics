518e9be112271f159d76f650bdbbcfa33f463384
zepumph <michael.kauzmann@colorado.edu>
2018-10-25 13:02:42 -0800

    move phetioInherit to tandem, https://github.com/phetsims/phet-io/issues/1401

--- js/common/view/control/VoltmeterToolboxPanelIO.js
+++ js/common/view/control/VoltmeterToolboxPanelIO.js
@@ -11,10 +11,10 @@ define( function( require ) {
   // modules
   var capacitorLabBasics = require( 'CAPACITOR_LAB_BASICS/capacitorLabBasics' );
   var NodeIO = require( 'SCENERY/nodes/NodeIO' );
+  var phetioInherit = require( 'TANDEM/phetioInherit' );
 
   // ifphetio
   var assertInstanceOf = require( 'ifphetio!PHET_IO/assertInstanceOf' );
-  var phetioInherit = require( 'ifphetio!PHET_IO/phetioInherit' );
 
   /**
    * IO type for phet/sun's AccordionBox class.
