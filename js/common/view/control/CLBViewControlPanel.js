9a2a5a4090c56f40be88db89a4ebe0f5d6e58082
phet-dev <phetadmin@gmail.com>
2020-02-06 03:09:47 -0700

    update copyright dates from daily grunt work

--- js/common/view/control/CLBViewControlPanel.js
+++ js/common/view/control/CLBViewControlPanel.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2019, University of Colorado Boulder
+// Copyright 2015-2020, University of Colorado Boulder
 
 /**
  * Control panel for view elements in Capacitor Lab: Basics.  Controls the visibility of plate charges, current
--- js/common/view/control/VoltmeterToolboxPanel.js
+++ js/common/view/control/VoltmeterToolboxPanel.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2019, University of Colorado Boulder
+// Copyright 2015-2020, University of Colorado Boulder
 
 /**
  * Toolbox that contains a voltmeter and timer.  The user can drag the voltmeter out of the toolbox for
