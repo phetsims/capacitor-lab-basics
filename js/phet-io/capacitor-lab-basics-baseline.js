0dc23f72f985edc8143be8a8e56feb4dec548158
chrisklus <christopher.klusendorf@colorado.edu>
2020-04-10 16:53:04 -0400

    Update baseline files, see https://github.com/phetsims/joist/issues/623

--- js/phet-io/capacitor-lab-basics-baseline.js
+++ js/phet-io/capacitor-lab-basics-baseline.js
@@ -4669,7 +4669,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioTypeName": "ActionIO<NumberIO, NumberIO>"
     },
     "capacitorLabBasics.general.model.screenProperty": {
-      "phetioDocumentation": "Which sim screen is selected, including the home screen",
+      "phetioDocumentation": "Determines which screen is selected in the simulation",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
       "phetioFeatured": true,
