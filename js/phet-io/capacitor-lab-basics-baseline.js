be61da910d99b200b795bc932d7b8b810114b36c
chrisklus <christopher.klusendorf@colorado.edu>
2020-04-16 22:20:56 -0400

    Update phet-io files, see https://github.com/phetsims/joist/issues/628

--- js/phet-io/capacitor-lab-basics-baseline.js
+++ js/phet-io/capacitor-lab-basics-baseline.js
@@ -6286,11 +6286,11 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioDocumentation": "Property for the displayed text",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
-      "phetioFeatured": true,
+      "phetioFeatured": false,
       "phetioHighFrequency": false,
       "phetioIsArchetype": false,
       "phetioPlayback": false,
-      "phetioReadOnly": false,
+      "phetioReadOnly": true,
       "phetioState": true,
       "phetioStudioControl": true,
       "phetioTypeName": "PropertyIO<StringIO>"
@@ -6307,6 +6307,19 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioState": true,
       "phetioStudioControl": true,
       "phetioTypeName": "PropertyIO<BooleanIO>"
+    },
+    "capacitorLabBasics.general.view.simNameProperty": {
+      "phetioDocumentation": "The name of the sim. Changing this value will update the title text on the navigation bar and the title text on the home screen, if it exists.",
+      "phetioDynamicElement": false,
+      "phetioEventType": "MODEL",
+      "phetioFeatured": true,
+      "phetioHighFrequency": false,
+      "phetioIsArchetype": false,
+      "phetioPlayback": false,
+      "phetioReadOnly": false,
+      "phetioState": true,
+      "phetioStudioControl": true,
+      "phetioTypeName": "PropertyIO<StringIO>"
     },
     "capacitorLabBasics.general.view.soundManager": {
       "phetioDocumentation": "Controls the simulation's sound. For sims that do not support sound, this element and its children can be ignored.",
@@ -6784,7 +6797,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioHighFrequency": false,
       "phetioIsArchetype": false,
       "phetioPlayback": false,
-      "phetioReadOnly": false,
+      "phetioReadOnly": true,
       "phetioState": true,
       "phetioStudioControl": true,
       "phetioTypeName": "PropertyIO<StringIO>"
