c069e8bc83d764c083f0525d2443776046f8ef97
chrisklus <christopher.klusendorf@colorado.edu>
2020-04-27 17:02:01 -0400

    Update phet-io files, see https://github.com/phetsims/joist/issues/628

--- js/phet-io/capacitor-lab-basics-baseline.js
+++ js/phet-io/capacitor-lab-basics-baseline.js
@@ -6308,7 +6308,7 @@ window.phet.preloads.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "PropertyIO<BooleanIO>"
     },
-    "capacitorLabBasics.general.view.simNameProperty": {
+    "capacitorLabBasics.general.model.simNameProperty": {
       "phetioDocumentation": "The name of the sim. Changing this value will update the title text on the navigation bar and the title text on the home screen, if it exists.",
       "phetioDynamicElement": false,
       "phetioEventType": "MODEL",
