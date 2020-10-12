6720349498d90ffb4e4586bf2e82ff655b8ce4e1
pixelzoom <cmalley@pixelzoom.com>
2020-10-12 15:29:41 -0600

    convert ScreenView subclasses to ES6 subclass, https://github.com/phetsims/tasks/issues/1044

--- js/capacitance/view/CapacitanceScreenView.js
+++ js/capacitance/view/CapacitanceScreenView.js
@@ -9,7 +9,6 @@
 
 import Vector2 from '../../../../dot/js/Vector2.js';
 import ScreenView from '../../../../joist/js/ScreenView.js';
-import inherit from '../../../../phet-core/js/inherit.js';
 import ResetAllButton from '../../../../scenery-phet/js/buttons/ResetAllButton.js';
 import StopwatchNode from '../../../../scenery-phet/js/StopwatchNode.js';
 import capacitorLabBasics from '../../capacitorLabBasics.js';
@@ -20,94 +19,94 @@ import DebugLayer from '../../common/view/DebugLayer.js';
 import VoltmeterNode from '../../common/view/meters/VoltmeterNode.js';
 import CapacitanceCircuitNode from './CapacitanceCircuitNode.js';
 
-/**
- * @constructor
- * @param {CLBModel} model
- * @param {Tandem} tandem
- */
-function CapacitanceScreenView( model, tandem ) {
+class CapacitanceScreenView extends ScreenView {
 
-  ScreenView.call( this, { tandem: tandem } );
+  /**
+   * @param {CLBModel} model
+   * @param {Tandem} tandem
+   */
+  constructor( model, tandem ) {
 
-  // @private {YawPitchModelViewTransform3}
-  this.modelViewTransform = model.modelViewTransform;
+    super( { tandem: tandem } );
 
-  // @private {CLBModel}
-  this.model = model;
+    // @private {YawPitchModelViewTransform3}
+    this.modelViewTransform = model.modelViewTransform;
 
-  // @private {CapacitanceCircuitNode} circuit
-  this.capacitanceCircuitNode = new CapacitanceCircuitNode( model, tandem.createTandem( 'capacitanceCircuitNode' ) );
+    // @private {CLBModel}
+    this.model = model;
 
-  // meters
-  const voltmeterNode = new VoltmeterNode( model.voltmeter, this.modelViewTransform, model.voltmeterVisibleProperty, tandem.createTandem( 'voltmeterNode' ) );
+    // @private {CapacitanceCircuitNode} circuit
+    this.capacitanceCircuitNode = new CapacitanceCircuitNode( model, tandem.createTandem( 'capacitanceCircuitNode' ) );
 
-  // @public {StopwatchNode}
-  const stopwatchNode = new StopwatchNode( model.stopwatch, {
-    visibleBoundsProperty: this.visibleBoundsProperty,
-    tandem: tandem.createTandem( 'stopwatchNode' ),
-    dragListenerOptions: {
-      end: () => {
+    // meters
+    const voltmeterNode = new VoltmeterNode( model.voltmeter, this.modelViewTransform, model.voltmeterVisibleProperty, tandem.createTandem( 'voltmeterNode' ) );
 
-        // When a node is released, check if it is over the toolbox.  If so, drop it in.
-        if ( toolboxPanel.bounds.intersectsBounds( stopwatchNode.bounds ) ) {
-          model.stopwatch.reset();
+    // @public {StopwatchNode}
+    const stopwatchNode = new StopwatchNode( model.stopwatch, {
+      visibleBoundsProperty: this.visibleBoundsProperty,
+      tandem: tandem.createTandem( 'stopwatchNode' ),
+      dragListenerOptions: {
+        end: () => {
+
+          // When a node is released, check if it is over the toolbox.  If so, drop it in.
+          if ( toolboxPanel.bounds.intersectsBounds( stopwatchNode.bounds ) ) {
+            model.stopwatch.reset();
+          }
         }
       }
-    }
-  } );
-  this.addChild( stopwatchNode );
+    } );
+    this.addChild( stopwatchNode );
 
-  const toolboxPanel = new VoltmeterToolboxPanel(
-    this.layoutBounds,
-    stopwatchNode,
-    voltmeterNode,
-    this.modelViewTransform,
-    model.voltmeter.isDraggedProperty,
-    model.stopwatch,
-    model.voltmeterVisibleProperty,
-    tandem.createTandem( 'toolboxPanel' ), {
-      includeTimer: false
-    }
-  );
+    const toolboxPanel = new VoltmeterToolboxPanel(
+      this.layoutBounds,
+      stopwatchNode,
+      voltmeterNode,
+      this.modelViewTransform,
+      model.voltmeter.isDraggedProperty,
+      model.stopwatch,
+      model.voltmeterVisibleProperty,
+      tandem.createTandem( 'toolboxPanel' ), {
+        includeTimer: false
+      }
+    );
 
-  // control
-  const viewControlPanel = new CLBViewControlPanel( model, tandem.createTandem( 'viewControlPanel' ), {
-    maxTextWidth: 200
-  } );
-  viewControlPanel.rightTop = this.layoutBounds.rightTop.plus( new Vector2( -10, 10 ) );
-  toolboxPanel.rightTop = viewControlPanel.rightBottom.plus( new Vector2( 0, 10 ) );
+    // control
+    const viewControlPanel = new CLBViewControlPanel( model, tandem.createTandem( 'viewControlPanel' ), {
+      maxTextWidth: 200
+    } );
+    viewControlPanel.rightTop = this.layoutBounds.rightTop.plus( new Vector2( -10, 10 ) );
+    toolboxPanel.rightTop = viewControlPanel.rightBottom.plus( new Vector2( 0, 10 ) );
 
-  const capacitanceBarMeterPanel = new BarMeterPanel(
-    model,
-    tandem.createTandem( 'barMeterPanel' )
-  );
-  capacitanceBarMeterPanel.left = this.capacitanceCircuitNode.topWireNode.left - 40;
-  capacitanceBarMeterPanel.top = this.layoutBounds.top + 10;
+    const capacitanceBarMeterPanel = new BarMeterPanel(
+      model,
+      tandem.createTandem( 'barMeterPanel' )
+    );
+    capacitanceBarMeterPanel.left = this.capacitanceCircuitNode.topWireNode.left - 40;
+    capacitanceBarMeterPanel.top = this.layoutBounds.top + 10;
 
-  const resetAllButton = new ResetAllButton( {
-    listener: function() {
-      model.reset();
-    },
-    bottom: this.layoutBounds.bottom - 30,
-    right: this.layoutBounds.right - 30,
-    radius: 25,
-    tandem: tandem.createTandem( 'resetAllButton' )
-  } );
+    const resetAllButton = new ResetAllButton( {
+      listener: function() {
+        model.reset();
+      },
+      bottom: this.layoutBounds.bottom - 30,
+      right: this.layoutBounds.right - 30,
+      radius: 25,
+      tandem: tandem.createTandem( 'resetAllButton' )
+    } );
 
-  // rendering order
-  this.addChild( this.capacitanceCircuitNode );
-  this.addChild( capacitanceBarMeterPanel );
-  this.addChild( viewControlPanel );
-  this.addChild( toolboxPanel );
-  this.addChild( voltmeterNode );
-  this.addChild( resetAllButton );
-  this.addChild( new DebugLayer( model ) );
+    // rendering order
+    this.addChild( this.capacitanceCircuitNode );
+    this.addChild( capacitanceBarMeterPanel );
+    this.addChild( viewControlPanel );
+    this.addChild( toolboxPanel );
+    this.addChild( voltmeterNode );
+    this.addChild( resetAllButton );
+    this.addChild( new DebugLayer( model ) );
 
-  // Explicitly set reset all button z-order
-  resetAllButton.moveToBack();
+    // Explicitly set reset all button z-order
+    resetAllButton.moveToBack();
+  }
 }
 
 capacitorLabBasics.register( 'CapacitanceScreenView', CapacitanceScreenView );
-
-inherit( ScreenView, CapacitanceScreenView );
 export default CapacitanceScreenView;
--- js/light-bulb/view/CLBLightBulbScreenView.js
+++ js/light-bulb/view/CLBLightBulbScreenView.js
@@ -9,7 +9,6 @@
 
 import Vector2 from '../../../../dot/js/Vector2.js';
 import ScreenView from '../../../../joist/js/ScreenView.js';
-import inherit from '../../../../phet-core/js/inherit.js';
 import ResetAllButton from '../../../../scenery-phet/js/buttons/ResetAllButton.js';
 import StopwatchNode from '../../../../scenery-phet/js/StopwatchNode.js';
 import TimeControlNode from '../../../../scenery-phet/js/TimeControlNode.js';
@@ -26,122 +25,121 @@ import DebugLayer from '../../common/view/DebugLayer.js';
 import VoltmeterNode from '../../common/view/meters/VoltmeterNode.js';
 import LightBulbCircuitNode from './LightBulbCircuitNode.js';
 
-/**
- * @constructor
- *
- * @param {CLBLightBulbModel} model
- * @param {Tandem} tandem
- */
-function CLBLightBulbScreenView( model, tandem ) {
+class CLBLightBulbScreenView extends ScreenView {
 
-  ScreenView.call( this, { tandem: tandem } );
+  /**
+   * @param {CLBLightBulbModel} model
+   * @param {Tandem} tandem
+   */
+  constructor( model, tandem ) {
 
-  // @private {YawPitchModelViewTransform3}
-  this.modelViewTransform = model.modelViewTransform;
+    super( { tandem: tandem } );
 
-  // @private {CLBLightBulbModel}
-  this.model = model;
+    // @private {YawPitchModelViewTransform3}
+    this.modelViewTransform = model.modelViewTransform;
 
-  // @private {LightBulbCircuitNode} Circuit
-  this.lightBulbCircuitNode = new LightBulbCircuitNode( model, tandem.createTandem( 'lightBulbCircuitNode' ) );
+    // @private {CLBLightBulbModel}
+    this.model = model;
 
-  // meters
-  const barMeterPanel = new BarMeterPanel( model, tandem.createTandem( 'barMeterPanel' ) );
-  const voltmeterNode = new VoltmeterNode( model.voltmeter, this.modelViewTransform, model.voltmeterVisibleProperty,
-    tandem.createTandem( 'voltmeterNode' ) );
+    // @private {LightBulbCircuitNode} Circuit
+    this.lightBulbCircuitNode = new LightBulbCircuitNode( model, tandem.createTandem( 'lightBulbCircuitNode' ) );
 
-  // @public {StopwatchNode}
-  const stopwatchNode = new StopwatchNode( model.stopwatch, {
-    visibleBoundsProperty: this.visibleBoundsProperty,
-    tandem: Tandem.OPT_OUT,// TODO(phet-io): this seems like it should not opt out, since it has interactive components
-    dragListenerOptions: {
-      end: () => {
+    // meters
+    const barMeterPanel = new BarMeterPanel( model, tandem.createTandem( 'barMeterPanel' ) );
+    const voltmeterNode = new VoltmeterNode( model.voltmeter, this.modelViewTransform, model.voltmeterVisibleProperty,
+      tandem.createTandem( 'voltmeterNode' ) );
 
-        // When a node is released, check if it is over the toolbox.  If so, drop it in.
-        if ( toolboxPanel.bounds.intersectsBounds( stopwatchNode.bounds ) ) {
-          model.stopwatch.reset();
+    // @public {StopwatchNode}
+    const stopwatchNode = new StopwatchNode( model.stopwatch, {
+      visibleBoundsProperty: this.visibleBoundsProperty,
+      tandem: Tandem.OPT_OUT,// TODO(phet-io): this seems like it should not opt out, since it has interactive components
+      dragListenerOptions: {
+        end: () => {
+
+          // When a node is released, check if it is over the toolbox.  If so, drop it in.
+          if ( toolboxPanel.bounds.intersectsBounds( stopwatchNode.bounds ) ) {
+            model.stopwatch.reset();
+          }
         }
       }
-    }
-  } );
-  this.addChild( stopwatchNode );
+    } );
+    this.addChild( stopwatchNode );
 
-  // @public {AlignGroup}
-  this.rightPanelAlignGroup = new AlignGroup( { matchVertical: false, minWidth: 350 } );
+    // @public {AlignGroup}
+    this.rightPanelAlignGroup = new AlignGroup( { matchVertical: false, minWidth: 350 } );
 
-  const toolboxPanel = new VoltmeterToolboxPanel(
-    this.layoutBounds,
-    stopwatchNode,
-    voltmeterNode,
-    this.modelViewTransform,
-    model.voltmeter.isDraggedProperty,
-    model.stopwatch,
-    model.voltmeterVisibleProperty,
-    tandem.createTandem( 'toolboxPanel' ), {
-      alignGroup: this.rightPanelAlignGroup
-    }
-  );
-
-  // View control panel and voltmeter panel
-  const viewControlPanel = new CLBViewControlPanel( model, tandem.createTandem( 'viewControlPanel' ), {
-    maxTextWidth: 200,
-    alignGroup: this.rightPanelAlignGroup
-  } );
-  viewControlPanel.rightTop = this.layoutBounds.rightTop.plus( new Vector2( -10, 10 ) );
-  toolboxPanel.rightTop = viewControlPanel.rightBottom.plus( new Vector2( 0, 10 ) );
-
-  // Circuit bar meter panel
-  barMeterPanel.left = this.lightBulbCircuitNode.topWireNode.left - 40;
-  barMeterPanel.top = this.layoutBounds.top + 10;
-
-  const timeControlPanel = new Panel( new TimeControlNode( model.isPlayingProperty, {
-    timeSpeedProperty: model.timeSpeedProperty,
-    playPauseStepButtonOptions: {
-      stepForwardButtonOptions: {
-        listener: function() { model.manualStep(); }
+    const toolboxPanel = new VoltmeterToolboxPanel(
+      this.layoutBounds,
+      stopwatchNode,
+      voltmeterNode,
+      this.modelViewTransform,
+      model.voltmeter.isDraggedProperty,
+      model.stopwatch,
+      model.voltmeterVisibleProperty,
+      tandem.createTandem( 'toolboxPanel' ), {
+        alignGroup: this.rightPanelAlignGroup
       }
-    },
-    tandem: tandem.createTandem( 'timeControlNode' )
-  } ), {
-    xMargin: 15,
-    yMargin: 15,
-    stroke: null,
-    fill: new Color( 255, 255, 255, 0.6 ),
-    tandem: tandem.createTandem( 'timeControlPanel' )
-  } );
+    );
 
-  const resetAllButton = new ResetAllButton( {
-    listener: function() {
-      model.reset();
-    },
-    radius: 25,
-    tandem: tandem.createTandem( 'resetAllButton' )
-  } );
+    // View control panel and voltmeter panel
+    const viewControlPanel = new CLBViewControlPanel( model, tandem.createTandem( 'viewControlPanel' ), {
+      maxTextWidth: 200,
+      alignGroup: this.rightPanelAlignGroup
+    } );
+    viewControlPanel.rightTop = this.layoutBounds.rightTop.plus( new Vector2( -10, 10 ) );
+    toolboxPanel.rightTop = viewControlPanel.rightBottom.plus( new Vector2( 0, 10 ) );
 
-  const simControlHBox = new HBox( {
-    children: [
-      timeControlPanel,
-      resetAllButton
-    ],
-    spacing: 50,
-    bottom: this.layoutBounds.bottom - 20,
-    right: this.layoutBounds.right - 30
-  } );
+    // Circuit bar meter panel
+    barMeterPanel.left = this.lightBulbCircuitNode.topWireNode.left - 40;
+    barMeterPanel.top = this.layoutBounds.top + 10;
 
-  // rendering order
-  this.addChild( this.lightBulbCircuitNode );
-  this.addChild( barMeterPanel );
-  this.addChild( viewControlPanel );
-  this.addChild( toolboxPanel );
-  this.addChild( voltmeterNode );
-  this.addChild( simControlHBox );
-  this.addChild( new DebugLayer( model ) );
+    const timeControlPanel = new Panel( new TimeControlNode( model.isPlayingProperty, {
+      timeSpeedProperty: model.timeSpeedProperty,
+      playPauseStepButtonOptions: {
+        stepForwardButtonOptions: {
+          listener: function() { model.manualStep(); }
+        }
+      },
+      tandem: tandem.createTandem( 'timeControlNode' )
+    } ), {
+      xMargin: 15,
+      yMargin: 15,
+      stroke: null,
+      fill: new Color( 255, 255, 255, 0.6 ),
+      tandem: tandem.createTandem( 'timeControlPanel' )
+    } );
 
-  // Explicitly set the sim controls z-order
-  simControlHBox.moveToBack();
+    const resetAllButton = new ResetAllButton( {
+      listener: function() {
+        model.reset();
+      },
+      radius: 25,
+      tandem: tandem.createTandem( 'resetAllButton' )
+    } );
+
+    const simControlHBox = new HBox( {
+      children: [
+        timeControlPanel,
+        resetAllButton
+      ],
+      spacing: 50,
+      bottom: this.layoutBounds.bottom - 20,
+      right: this.layoutBounds.right - 30
+    } );
+
+    // rendering order
+    this.addChild( this.lightBulbCircuitNode );
+    this.addChild( barMeterPanel );
+    this.addChild( viewControlPanel );
+    this.addChild( toolboxPanel );
+    this.addChild( voltmeterNode );
+    this.addChild( simControlHBox );
+    this.addChild( new DebugLayer( model ) );
+
+    // Explicitly set the sim controls z-order
+    simControlHBox.moveToBack();
+  }
 }
 
 capacitorLabBasics.register( 'CLBLightBulbScreenView', CLBLightBulbScreenView );
-
-inherit( ScreenView, CLBLightBulbScreenView );
 export default CLBLightBulbScreenView;
