abfca7da5055b17e1225e99045d97b618d554636
pixelzoom <cmalley@pixelzoom.com>
2020-10-13 10:50:30 -0600

    convert to ArrowNode and its subclasses to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/common/view/drag/DragHandleArrowNode.js
+++ js/common/view/drag/DragHandleArrowNode.js
@@ -7,7 +7,6 @@
  * @author Jesse Greenberg (PhET Interactive Simulations)
  */
 
-import inherit from '../../../../../phet-core/js/inherit.js';
 import ArrowNode from '../../../../../scenery-phet/js/ArrowNode.js';
 import capacitorLabBasics from '../../../capacitorLabBasics.js';
 
@@ -17,44 +16,42 @@ const HIGHLIGHT_COLOR = 'yellow';
 const STROKE_COLOR = 'black';
 const LINE_WIDTH = 1;
 
-/**
- * @constructor
- *
- * @param {Vector2} pStart
- * @param {Vector2} pEnd
- * @param {Property.<boolean>} isHighlightedProperty
- * @param {Tandem} tandem
- */
-function DragHandleArrowNode( pStart, pEnd, isHighlightedProperty, tandem ) {
-  const self = this;
+class DragHandleArrowNode extends ArrowNode {
 
-  // calculate the parameter for head and tail width and height.
-  const length = Math.abs( pStart.distance( pEnd ) );
+  /**
+   * @param {Vector2} pStart
+   * @param {Vector2} pEnd
+   * @param {Property.<boolean>} isHighlightedProperty
+   * @param {Tandem} tandem
+   */
+  constructor( pStart, pEnd, isHighlightedProperty, tandem ) {
 
-  ArrowNode.call( this, pStart.x, pStart.y, pEnd.x, pEnd.y, {
-    fill: NORMAL_COLOR,
-    stroke: STROKE_COLOR,
-    lineWidth: LINE_WIDTH,
-    doubleHead: true,
-    headHeight: length,
-    headWidth: length / 2,
-    tailWidth: length / 5,
-    tandem: tandem
-  } );
+    // calculate the parameter for head and tail width and height.
+    const length = Math.abs( pStart.distance( pEnd ) );
 
-  this.normalColor = NORMAL_COLOR;
-  this.highlightColor = HIGHLIGHT_COLOR;
+    super( pStart.x, pStart.y, pEnd.x, pEnd.y, {
+      fill: NORMAL_COLOR,
+      stroke: STROKE_COLOR,
+      lineWidth: LINE_WIDTH,
+      doubleHead: true,
+      headHeight: length,
+      headWidth: length / 2,
+      tailWidth: length / 5,
+      tandem: tandem
+    } );
 
-  // make the arrow slightly easier to drag
-  this.touchArea = this.bounds.dilated( 10 );
+    this.normalColor = NORMAL_COLOR;
+    this.highlightColor = HIGHLIGHT_COLOR;
 
-  // highlight the arrow on pointer over
-  isHighlightedProperty.link( function( highlighted ) {
-    self.fill = highlighted ? HIGHLIGHT_COLOR : NORMAL_COLOR;
-  } );
+    // make the arrow slightly easier to drag
+    this.touchArea = this.bounds.dilated( 10 );
+
+    // highlight the arrow on pointer over
+    isHighlightedProperty.link( highlighted => {
+      this.fill = highlighted ? HIGHLIGHT_COLOR : NORMAL_COLOR;
+    } );
+  }
 }
 
 capacitorLabBasics.register( 'DragHandleArrowNode', DragHandleArrowNode );
-
-inherit( ArrowNode, DragHandleArrowNode );
 export default DragHandleArrowNode;
