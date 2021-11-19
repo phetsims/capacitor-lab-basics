efc22eadf5537e2435d0a0153d1fb001efd13536
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:34 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/capacitorLabBasicsStrings.ts
@@ -0,0 +1,45 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import capacitorLabBasics from './capacitorLabBasics.js';
+
+type StringsType = {
+  'capacitance': string,
+  'conventional': string,
+  'currentDirection': string,
+  'barGraphs': string,
+  'capacitor-lab-basics': {
+    'title': string
+  },
+  'screen': {
+    'capacitance': string,
+    'lightBulb': string
+  },
+  'electricField': string,
+  'electrons': string,
+  'plateArea': string,
+  'topPlateCharge': string,
+  'plateCharges': string,
+  'separation': string,
+  'storedEnergy': string,
+  'voltage': string,
+  'voltsPattern': string,
+  'millimetersPattern': string,
+  'millimetersSquaredPattern': string,
+  'picoJoulesPattern': string,
+  'picoFaradsPattern': string,
+  'picoCoulombsPattern': string,
+  'volts': {
+    'unknown': string
+  }
+};
+
+const capacitorLabBasicsStrings = getStringModule( 'CAPACITOR_LAB_BASICS' ) as StringsType;
+
+capacitorLabBasics.register( 'capacitorLabBasicsStrings', capacitorLabBasicsStrings );
+
+export default capacitorLabBasicsStrings;
