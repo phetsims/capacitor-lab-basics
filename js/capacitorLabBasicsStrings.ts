f9921fcc3c438eec38ad79e8cd1232b23c430b6e
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:35 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/capacitorLabBasicsStrings.ts
+++ js/capacitorLabBasicsStrings.ts
@@ -5,36 +5,59 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import capacitorLabBasics from './capacitorLabBasics.js';
 
 type StringsType = {
   'capacitance': string;
+  'capacitanceProperty': TReadOnlyProperty<string>;
   'conventional': string;
+  'conventionalProperty': TReadOnlyProperty<string>;
   'currentDirection': string;
+  'currentDirectionProperty': TReadOnlyProperty<string>;
   'barGraphs': string;
+  'barGraphsProperty': TReadOnlyProperty<string>;
   'capacitor-lab-basics': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'screen': {
     'capacitance': string;
+    'capacitanceProperty': TReadOnlyProperty<string>;
     'lightBulb': string;
+    'lightBulbProperty': TReadOnlyProperty<string>;
   };
   'electricField': string;
+  'electricFieldProperty': TReadOnlyProperty<string>;
   'electrons': string;
+  'electronsProperty': TReadOnlyProperty<string>;
   'plateArea': string;
+  'plateAreaProperty': TReadOnlyProperty<string>;
   'topPlateCharge': string;
+  'topPlateChargeProperty': TReadOnlyProperty<string>;
   'plateCharges': string;
+  'plateChargesProperty': TReadOnlyProperty<string>;
   'separation': string;
+  'separationProperty': TReadOnlyProperty<string>;
   'storedEnergy': string;
+  'storedEnergyProperty': TReadOnlyProperty<string>;
   'voltage': string;
+  'voltageProperty': TReadOnlyProperty<string>;
   'voltsPattern': string;
+  'voltsPatternProperty': TReadOnlyProperty<string>;
   'millimetersPattern': string;
+  'millimetersPatternProperty': TReadOnlyProperty<string>;
   'millimetersSquaredPattern': string;
+  'millimetersSquaredPatternProperty': TReadOnlyProperty<string>;
   'picoJoulesPattern': string;
+  'picoJoulesPatternProperty': TReadOnlyProperty<string>;
   'picoFaradsPattern': string;
+  'picoFaradsPatternProperty': TReadOnlyProperty<string>;
   'picoCoulombsPattern': string;
+  'picoCoulombsPatternProperty': TReadOnlyProperty<string>;
   'volts': {
     'unknown': string;
+    'unknownProperty': TReadOnlyProperty<string>;
   }
 };
 
