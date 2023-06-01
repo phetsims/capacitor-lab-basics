d24e7c48da4b9722f6443991b79daccf6edceb4c
phet-dev <phetadmin@gmail.com>
2023-06-01 03:26:51 -0600

    update copyright dates from daily grunt work

--- js/common/model/Capacitor.js
+++ js/common/model/Capacitor.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2022, University of Colorado Boulder
+// Copyright 2015-2023, University of Colorado Boulder
 
 /**
  * Model of a capacitor whose geometry can be directly manipulated by the user.
