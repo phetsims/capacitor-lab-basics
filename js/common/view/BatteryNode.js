fe81ed61d5bb9ee6a436cf26f41567fac2a253b8
phet-dev <phetadmin@gmail.com>
2023-09-02 03:12:08 -0600

    update copyright dates from daily grunt work

--- js/common/view/BatteryNode.js
+++ js/common/view/BatteryNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2022, University of Colorado Boulder
+// Copyright 2014-2023, University of Colorado Boulder
 
 /**
  * Visual representation of a DC battery, with a control for setting its voltage. Image flips when the polarity of the
